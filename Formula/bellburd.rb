00f6ef023210af8b0424d5c8c65719f76959a004
Trevor <trevoranthonylane@gmail.com>
2026-02-10 21:36:53 -0700

    Point formula to public release asset

--- Formula/bellburd.rb
+++ Formula/bellburd.rb
@@ -1,7 +1,7 @@
 class Bellburd < Formula
   desc "Bellburd CLI for on-call management"
   homepage "https://github.com/trevorhere/bellburd"
-  url "https://github.com/trevorhere/bellburd/releases/download/v0.1.0/bellburd-cli-0.1.0.tgz"
+  url "https://github.com/trevorhere/homebrew-bellburd/releases/download/v0.1.0/bellburd-cli-0.1.0.tgz"
   sha256 "c464e63b83322bfcbb28257b3ce23d840709fa5796ed3c72fba92f51945e8a88"
   license "UNLICENSED"
 
